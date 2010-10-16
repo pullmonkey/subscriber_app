cb722f61285b41d5c094af0d0c29d9fd97360219
PullMonkey <charles.snider@skizmo.com>
2010-10-16 14:33:53 -0600

    subscriber app and plugin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SubscriberApp::Application.config.secret_token = '91d9876b84c4b7b4b3fd03e37f644efd9e187e822a9d86958d7b469231203b7144b7168ba80f30353b9440be60c59a1f6f903937f132e0dea235a9a3e25f0427'
